d81505b69f5a8e80ee4ea4113e14d30d90adfc80
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-11-04 11:19:51 +0100

    Cleanup create react app

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,9 @@
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello world/i);
   expect(linkElement).toBeInTheDocument();
 });
